7f64432f7e51fae5afee31b893eda6ec418ef9be
Antoine Poullet <antoine.poullet@gearset.com>
2021-09-27 10:16:18 +0100

    Trying to make the test fail again.

--- force-app/main/default/lwc/hello/__tests__/hello.test.js
+++ force-app/main/default/lwc/hello/__tests__/hello.test.js
@@ -18,7 +18,7 @@ describe('c-hello', () => {
 
         // Verify displayed greeting
         const div = element.shadowRoot.querySelector('div');
-        expect(div.textContent).toBe('Something Wrong.');
+        fail("This test should fail.")
     });
 
     it('is accessible', async () => {
